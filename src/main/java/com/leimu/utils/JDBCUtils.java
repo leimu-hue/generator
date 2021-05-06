22956e60e1de260c75748b6e0510274f4ac283ce
leimu-hue <2627492139@qq.com>
2021-05-06 09:45:40 +0800

    删除没用的方法 2021-04-17

--- src/main/java/com/leimu/utils/JDBCUtils.java
+++ src/main/java/com/leimu/utils/JDBCUtils.java
@@ -110,38 +110,4 @@ public class JDBCUtils {
 
         return tableConstant;
     }
-
-    /**
-     * 解析表中的字段
-     *
-     * @param table
-     */
-    public static void resolveColumn(String table) {
-        Connection connection = null;
-        try {
-            connection = getConnection();
-        } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            logger.error(e.getMessage(), e);
-            e.printStackTrace();
-        }
-        try {
-            table = table.trim();
-            if (table.contains(" ")) {
-                logger.error("表 " + table + " 一个单词，请检查");
-                return;
-            }
-            PreparedStatement preparedStatement = connection.prepareStatement("desc " + table);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-
-            }
-        } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
-            e.printStackTrace();
-        }
-
-    }
 }
