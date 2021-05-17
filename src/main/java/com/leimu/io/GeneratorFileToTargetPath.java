ce9baf9312c5734b9823f730140f83b38b90cf42
leimu-hue <2627492139@qq.com>
2021-05-17 23:27:29 +0800

    编写了生成类的代码 2021-04-17

--- src/main/java/com/leimu/io/GeneratorFileToTargetPath.java
+++ src/main/java/com/leimu/io/GeneratorFileToTargetPath.java
@@ -7,7 +7,6 @@ import com.leimu.database.detail.TableDetail;
 import com.leimu.utils.FileContentGeneratorUtils;
 import com.leimu.utils.FileWriteUtils;
 import com.leimu.utils.StringUtils;
-import sun.net.www.protocol.file.FileURLConnection;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -18,11 +17,26 @@ public class GeneratorFileToTargetPath {
 
     private static String entityOfPackage;
 
+    private static String entityOfName;
+
+    /**
+     * 基础的名字
+     * 比如Generator
+     * 实体类为GeneratorEntity
+     */
+    private static String baseJavaName;
+
     private static String controllerPackage;
+
+    private static String controllerOfName;
 
     private static String servicePackage;
 
+    private static String serviceOfName;
+
     private static String mapperPackage;
+
+    private static String mapperOfName;
 
     private static String outputFilePath;
 
@@ -38,22 +52,65 @@ public class GeneratorFileToTargetPath {
         GeneratorFileToTargetPath.fileBuilderOfConfig = fileBuilderOfConfig;
         List<TableDetail> tableDetails = tableConstants.getTableDetails();
         tableDetails.forEach(o -> {
-            //生成实体类
-            try {
-                generatorEntity(o);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.exit(0);
+            //在这里需要将所有需要生成的类 包名和类名设置完毕
+            if(setAllPackageAndClassName(o.getTableName())){
+                //生成实体类
+                try {
+                    generatorEntity(o);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(0);
+                }
+                //生成mapper层
+                try {
+                    generatorMapper(o);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //生成mapper.xml文件
+                generatorMapperXML(o);
+                //生成service层
+                try {
+                    generatorService(o);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //生成controller层
+                try {
+                    generatorController(o);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            //生成mapper层
-            generatorMapper(o);
-            //生成mapper.xml文件
-            generatorMapperXML(o);
-            //生成service层
-            generatorService(o);
-            //生成controller层
-            generatorController(o);
         });
+    }
+
+    /**
+     * 设置生成属性的基本信息
+     * @param tableName 表的名字
+     * @return 返回当前的表名字是否为空
+     */
+    private static boolean setAllPackageAndClassName(String tableName){
+        if (StringUtils.isEmpty(tableName)){
+            return false;
+        }
+        tableName = StringUtils.toConvertName(tableName);
+        String basePackage = fileBuilderOfConfig.getBasePackage();
+        //设置基础名字
+        GeneratorFileToTargetPath.baseJavaName = tableName;
+        //设置实体类的基础信息
+        GeneratorFileToTargetPath.entityOfPackage = basePackage + ".entity";
+        GeneratorFileToTargetPath.entityOfName = tableName + "Entity.java";
+        //生成mapper的基础信息
+        GeneratorFileToTargetPath.mapperPackage = basePackage + ".mapper";
+        GeneratorFileToTargetPath.mapperOfName = tableName + "Mapper.java";
+        //生成service的基础信息
+        GeneratorFileToTargetPath.servicePackage = basePackage + ".service";
+        GeneratorFileToTargetPath.serviceOfName = tableName + "Service.java";
+        //生成controller的基础信息
+        GeneratorFileToTargetPath.controllerPackage = basePackage + ".controller";
+        GeneratorFileToTargetPath.controllerOfName = tableName + "Controller.java";
+        return true;
     }
 
     /**
@@ -62,17 +119,15 @@ public class GeneratorFileToTargetPath {
      * @param tableDetail 需要生成的内容
      */
     private static void generatorEntity(TableDetail tableDetail) throws IOException {
-        String tableName = StringUtils.toConvertName(tableDetail.getTableName());
-        String basePackage = fileBuilderOfConfig.getBasePackage();
         //设置路径
-        String path = FileWriteUtils.createPath(outputFilePath, basePackage + ".entity");
+        String path = FileWriteUtils.createPath(outputFilePath, GeneratorFileToTargetPath.entityOfPackage);
         //设置输出流路径
-        FileWriteUtils.setOutputPath(path,tableName+".java");
+        FileWriteUtils.setOutputPath(path,GeneratorFileToTargetPath.entityOfName);
         BufferedWriter write = FileWriteUtils.getWrite();
         //首先写入package内容
-        if (!StringUtils.isEmpty(basePackage)){
-            String packageContent = FileContentGeneratorUtils.generatorPackageContent(basePackage + ".entity");
-            entityOfPackage = packageContent.replace("package ","");
+        if (!StringUtils.isEmpty(GeneratorFileToTargetPath.entityOfPackage)){
+            String packageContent = FileContentGeneratorUtils.
+                    generatorPackageContent(GeneratorFileToTargetPath.entityOfPackage);
             FileWriteUtils.toWriteLineContentInFile(write,packageContent);
         }
         //其次生成import的内容
@@ -81,7 +136,8 @@ public class GeneratorFileToTargetPath {
             FileWriteUtils.toWriteLineContentInFile(write,importContent);
         }
         //生成文件内容
-        String contentEntity = "public class "+tableName + "{";
+        String contentEntity = "public class " + GeneratorFileToTargetPath.
+                entityOfName.substring(0,entityOfName.length()-5) + "{";
         FileWriteUtils.toWriteLineContentInFile(write,contentEntity);
         //输出字段
         List<String> setAndGetGenerator = new ArrayList<>();
@@ -111,9 +167,34 @@ public class GeneratorFileToTargetPath {
      *
      * @param tableDetail 需要生成的内容
      */
-    private static void generatorMapper(TableDetail tableDetail) {
+    private static void generatorMapper(TableDetail tableDetail) throws IOException {
+        //设置路径
+        String path = FileWriteUtils.createPath(outputFilePath,
+                GeneratorFileToTargetPath.mapperPackage);
+        //设置输出流路径
+        FileWriteUtils.setOutputPath(path,GeneratorFileToTargetPath.mapperOfName);
+        BufferedWriter write = FileWriteUtils.getWrite();
+        //首先写入package内容
+        if (!StringUtils.isEmpty(GeneratorFileToTargetPath.mapperPackage)){
+            String packageContent = FileContentGeneratorUtils.generatorPackageContent(GeneratorFileToTargetPath.mapperPackage);
+            FileWriteUtils.toWriteLineContentInFile(write,packageContent);
+        }
+        //生成import内容
 
+        //生成文件内容
+        String contentMapper = "public interface " + GeneratorFileToTargetPath.
+                mapperOfName.substring(0,mapperOfName.length()-5) + "{";
+        FileWriteUtils.toWriteLineContentInFile(write,contentMapper);
+
+        //生成方法体
+
+
+        //结尾
+        FileWriteUtils.toWriteLineContentInFile(write,"}");
+        write.flush();
+        write.close();
     }
+
 
     /**
      * 生成mapper xml文件
@@ -129,8 +210,32 @@ public class GeneratorFileToTargetPath {
      *
      * @param tableDetail 需要生成的内容
      */
-    private static void generatorService(TableDetail tableDetail) {
+    private static void generatorService(TableDetail tableDetail) throws IOException {
+        //设置路径
+        String path = FileWriteUtils.createPath(outputFilePath,
+                GeneratorFileToTargetPath.servicePackage);
+        //设置输出流路径
+        FileWriteUtils.setOutputPath(path,GeneratorFileToTargetPath.serviceOfName);
+        BufferedWriter write = FileWriteUtils.getWrite();
+        //首先写入package内容
+        if (!StringUtils.isEmpty(GeneratorFileToTargetPath.servicePackage)){
+            String packageContent = FileContentGeneratorUtils.generatorPackageContent(GeneratorFileToTargetPath.servicePackage);
+            FileWriteUtils.toWriteLineContentInFile(write,packageContent);
+        }
+        //生成import内容
 
+        //生成文件内容
+        String contentMapper = "public interface " + GeneratorFileToTargetPath.
+                serviceOfName.substring(0,serviceOfName.length()-5) + "{";
+        FileWriteUtils.toWriteLineContentInFile(write,contentMapper);
+
+        //生成方法体
+
+
+        //结尾
+        FileWriteUtils.toWriteLineContentInFile(write,"}");
+        write.flush();
+        write.close();
     }
 
     /**
@@ -138,8 +243,32 @@ public class GeneratorFileToTargetPath {
      *
      * @param tableDetail 需要生成的内容
      */
-    private static void generatorController(TableDetail tableDetail) {
+    private static void generatorController(TableDetail tableDetail) throws IOException {
+        //设置路径
+        String path = FileWriteUtils.createPath(outputFilePath,
+                GeneratorFileToTargetPath.controllerPackage);
+        //设置输出流路径
+        FileWriteUtils.setOutputPath(path,GeneratorFileToTargetPath.controllerOfName);
+        BufferedWriter write = FileWriteUtils.getWrite();
+        //首先写入package内容
+        if (!StringUtils.isEmpty(GeneratorFileToTargetPath.controllerPackage)){
+            String packageContent = FileContentGeneratorUtils.generatorPackageContent(GeneratorFileToTargetPath.controllerPackage);
+            FileWriteUtils.toWriteLineContentInFile(write,packageContent);
+        }
+        //生成import内容
 
+        //生成文件内容
+        String contentMapper = "public interface " + GeneratorFileToTargetPath.
+                controllerOfName.substring(0,controllerOfName.length()-5) + "{";
+        FileWriteUtils.toWriteLineContentInFile(write,contentMapper);
+
+        //生成方法体
+
+
+        //结尾
+        FileWriteUtils.toWriteLineContentInFile(write,"}");
+        write.flush();
+        write.close();
     }
 
 }
